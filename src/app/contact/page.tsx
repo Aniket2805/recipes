3f5478f985d2c16b3ac2c22904411d1b6d732493
Aniket2805 <aniketrocks2468@gmail.com>
2024-06-02 13:47:26 +0530

    w

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,7 +11,13 @@ import { cn } from "@/utils/cn";
 const page = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Form submitted");
+    toast("Form Submitted", {
+      description: "Sunday, December 03, 2023 at 9:00 AM",
+      action: {
+        label: "Remove",
+        onClick: () => console.log("Undo"),
+      },
+    });
   };
   const BottomGradient = () => {
     return (
@@ -77,15 +83,25 @@ const page = () => {
             <h2 className="font-bold text-xl text-neutral-800">
               Send us a message
             </h2>
-            <form className="my-8" onSubmit={handleSubmit}>
+            <form className="my-8" onSubmit={handleSubmit} method="POST">
               <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
                 <LabelInputContainer>
                   <Label htmlFor="firstname">First name</Label>
-                  <Input id="firstname" placeholder="Tyler" type="text" />
+                  <Input
+                    id="firstname"
+                    placeholder="Tyler"
+                    type="text"
+                    required
+                  />
                 </LabelInputContainer>
                 <LabelInputContainer>
                   <Label htmlFor="lastname">Last name</Label>
-                  <Input id="lastname" placeholder="Durden" type="text" />
+                  <Input
+                    id="lastname"
+                    placeholder="Durden"
+                    type="text"
+                    required
+                  />
                 </LabelInputContainer>
               </div>
               <LabelInputContainer className="mb-4">
@@ -94,6 +110,7 @@ const page = () => {
                   id="email"
                   placeholder="projectmayhem@fc.com"
                   type="email"
+                  required
                 />
               </LabelInputContainer>
               <LabelInputContainer className="mb-8">
@@ -102,20 +119,12 @@ const page = () => {
                   id="twitterpassword"
                   placeholder="Your Message"
                   type="twitterpassword"
+                  required
                 />
               </LabelInputContainer>
               <button
                 className="bg-gradient-to-br relative group/btn from-black to-neutral-600 block w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset]"
                 type="submit"
-                onClick={() =>
-                  toast("Form Submitted", {
-                    description: "Sunday, December 03, 2023 at 9:00 AM",
-                    action: {
-                      label: "Remove",
-                      onClick: () => console.log("Undo"),
-                    },
-                  })
-                }
               >
                 Send &rarr;
                 <BottomGradient />
