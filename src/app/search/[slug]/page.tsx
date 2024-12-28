d119bac75a8c9d64dcf642716e575af4f017bd56
Aniket2805 <aniketrocks2468@gmail.com>
2024-12-28 12:54:56 +0530

    ww

--- src/app/search/[slug]/page.tsx
+++ src/app/search/[slug]/page.tsx
@@ -12,12 +12,12 @@ const Page = () => {
       .then((data) => {
         setrecipes(data.meals);
       });
-  }, []);
+  }, [slug]);
   return (
     <div className="my-10 pt-16 sm:pt-20 px-8 sm:px-16 lg:px-24">
       <div>
         <h1 className="text-yellow-400 text-center text-4xl font-semibold font-mono mb-8">
-          Search results for "{slug}"
+          Search results for '{slug}'
         </h1>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-4">
